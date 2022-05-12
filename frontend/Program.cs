350d2c77826584d6f95d749d78164e3c4efb02b3
TuffenDuffen <reg_i@wext.se>
2022-05-12 17:07:41 +0200

    added frontend project

---
+++ frontend/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace frontend 
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("---------------PassManger---------------\n" +
+                              "Input master password: ");
+            Console.WriteLine("\nkey: " + Console.ReadKey(true).KeyChar);
+        }
+    }
+}
