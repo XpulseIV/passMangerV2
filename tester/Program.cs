b7f4816f2a9ad6daf45a4c138f7862d7567c1c38
XpulseIV <malteaxner363@gmail.com>
2022-05-12 16:39:39 +0200

    first commit

---
+++ tester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tester // Note: actual namespace depends on the project name.
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Gello World!");
+        }
+    }
+}
